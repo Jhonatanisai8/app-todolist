0793e27b809da76e8631ba21ec27be8cd65df374
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-08-14 00:11:52 -0500

    feat: implementacion del metodo para listar, agregar y eliminar tareas

--- backend/src/main/java/com/isai/backend/repository/TareaRepository.java
+++ backend/src/main/java/com/isai/backend/repository/TareaRepository.java
@@ -1,9 +1,10 @@
 package com.isai.backend.repository;
 
+import com.isai.backend.models.entities.Tarea;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TareaRepository
-        extends CrudRepository {
+        extends CrudRepository<Tarea, Long> {
 }
--- backend/src/main/java/com/isai/backend/service/TareaServiceIMPL.java
+++ backend/src/main/java/com/isai/backend/service/TareaServiceIMPL.java
@@ -1,28 +1,42 @@
 package com.isai.backend.service;
 
+import com.isai.backend.mappers.TareaMapper;
 import com.isai.backend.models.dtos.TareaDTOReq;
 import com.isai.backend.models.dtos.TareaDTORes;
+import com.isai.backend.models.entities.Tarea;
+import com.isai.backend.repository.TareaRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TareaServiceIMPL
         implements ITareaService {
 
+    private final TareaRepository tareaRepository;
+
     @Override
     public List<TareaDTORes> listarTareas() {
-        return List.of();
+        List<Tarea> tareas = new ArrayList<>();
+        tareaRepository.findAll().forEach(tareas::add);
+        return tareas.stream()
+                .map(TareaMapper::toDTO)
+                .toList();
     }
 
     @Override
     public TareaDTOReq registrarTarea(TareaDTOReq tareaDTOReq) {
-        return null;
+        Tarea tarea = TareaMapper.toEntity(tareaDTOReq);
+        tareaRepository.save(tarea);
+        return tareaDTOReq;
     }
 
     @Override
     public void eliminarTarea(Long id) {
-
+        tareaRepository.deleteById(id);
     }
 
 
