e44d798b8e06d637cfe4ff485d001735f8942f8e
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-08-13 23:54:04 -0500

    feat: repositorio para la entidad tarea

---
+++ backend/src/main/java/com/isai/backend/repository/TareaRepository.java
@@ -0,0 +1,9 @@
+package com.isai.backend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TareaRepository
+        extends CrudRepository {
+}
