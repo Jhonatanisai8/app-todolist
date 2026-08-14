b15d629a897fc85a33ad363b5f10732f50e0dbb7
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-08-14 00:24:13 -0500

    feat: en los dtos agregar anotacion como AllArgsConstructor y NoArgsConstructor

--- backend/src/main/java/com/isai/backend/models/dtos/TareaDTOReq.java
+++ backend/src/main/java/com/isai/backend/models/dtos/TareaDTOReq.java
@@ -1,12 +1,16 @@
 package com.isai.backend.models.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TareaDTOReq {
     private String titulo;
-    private Boolean completado = false;
+    private Boolean completado;
 
 }
--- backend/src/main/java/com/isai/backend/models/dtos/TareaDTORes.java
+++ backend/src/main/java/com/isai/backend/models/dtos/TareaDTORes.java
@@ -1,10 +1,16 @@
 package com.isai.backend.models.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
+@Getter
 public class TareaDTORes {
     private Long idTare;
     private String titulo;
-    private Boolean completado = false;
+    private Boolean completado;
 }
