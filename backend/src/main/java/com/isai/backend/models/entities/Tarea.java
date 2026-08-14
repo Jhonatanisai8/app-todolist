d71b4cd9abf2ac9eebc5d957599e88d81ec69961
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-08-13 23:50:41 -0500

    feat: entidad Tarea

--- backend/src/main/java/com/isai/backend/models/entities/Tarea.java
+++ backend/src/main/java/com/isai/backend/models/entities/Tarea.java
@@ -1,9 +1,27 @@
 package com.isai.backend.models.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 /**
  * Tarea
  */
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Tarea {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idTare;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private Boolean completado = false;
+
 }
